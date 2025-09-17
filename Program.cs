4c42a84b1041e1d342bd60d89684508decb873a6
dragavit <dragavit@gmail.com>
2025-09-18 01:21:35 +0300

    Initial commit: file I/O example

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿global using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите путь к файлу:");
+        string filePath = Console.ReadLine();
+
+        if (!File.Exists(filePath))
+        {
+            Console.WriteLine("Файла здесь нет/ Создаю новый");
+            File.WriteAllText(filePath, ""); // создаем пустой файл
+        }
+
+        Console.WriteLine("\n=== Содержимое файла ===\"");
+        string content = File.ReadAllText(filePath);
+        Console.WriteLine(content);
+
+        Console.WriteLine("\n=== Запишем инфу ===\"");
+        File.WriteAllText(filePath, "Запись произведена!");
+
+        Console.WriteLine("\n=== Новая инфа ===\"");
+        content = File.ReadAllText(filePath);
+        Console.WriteLine(content);
+
+
+
+
+    }
+}
+
